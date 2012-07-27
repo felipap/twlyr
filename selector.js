e965ce242ac76d2489f0cfc17622144293d2c883
Felipe <pires.a.felipe@gmail.com>
2012-07-27 05:01:22 -0300

    much done

--- selector.js
+++ selector.js
@@ -9,65 +9,9 @@
 
 		unselectWords: function () {
 			// Unselect words, by removing their "selected" class.
-
-			selected = document.querySelectorAll('.word.selected') 
+			var selected = document.querySelectorAll('.word.selected') 
 			for (var i=0; i<selected.length; i++)
 				selected[i].classList.remove('selected')
-		}
-
-		, getRangeSorted: function ( a, b ) {
-			var words = Array.prototype.slice.call(document.querySelectorAll('.word')),
-				verses = document.querySelector(".verse"),
-				selection = []
-
-			if (words.indexOf(a) < words.indexOf(b))
-				var first = a, last = b
-			else
-				var first = b, last = a
-
-			getRange(first, last)
-
-		}
-		, getRange: function ( a, b ) {
-			// return the list of words between (an including) A and B
-
-			
-			if (!a || !b)
-				return []
-			
-			words = Array.prototype.slice.call(document.querySelectorAll('.word'))
-			ia = words.indexOf(a)
-			ib = words.indexOf(b)
-			
-			if (ia < ib)
-				var first = ia, last = ib
-			else if (ia > ib)
-				var first = ib, last = ia
-			else // ia === ib
-				return [a]
-
-			return words.slice(first, last+1)
-
-			
-			var w = document.querySelectorAll('.word'),
-				selection = [],
-				last = null // the item that come at last
-
-			for (var i=0; i<w.length; i++) {
-				if (w[i] == a)
-					last = b;
-				else if (w[i] == b)
-					last = a;
-
-				if (last !== null)
-					for (; i<w.length; i++) {
-						selection.push(w[i])
-						if (w[i] == last)
-							return selection
-					}
-			}
-
-			return []
 		}
 
 		, selectRange: function ( a, b ) {
@@ -80,32 +24,63 @@
 				throw Error('no range selected!')
 		}
 
-		, getSelectionEnds: function ( actual ) {
-			// returns the extreme words in current selection process.
-			// optional argument 'actual', to be used instead of
-			// 'endWord'. this is essential when only one word is selected.
-			if ((!hoverWord && !actual) || !endWord) {
-				// if not in selection process, try to get selected words.
-				// Selector.is what works when words are selected by no longer
-				// BEING selected = mouse is up.
-				var w = document.querySelectorAll('.word.selected')
-				return [w[0], w[w.length-1]]
+		, getSplitRange: function ( a, b ) {
+			// Get list of words in the range, divided according to the lines they're in.
+
+			var words = Selector.getRange(a, b)
+				, selection = []
+				, i = 0
+			while (w = words[i++]) {
+				
+				if (!selection[0] || (words[i-2].parentElement !== w.parentElement))
+					selection.push([w])
+				else
+					selection[selection.length-1].push(w)
 			}
-			return [hoverWord, actual || endWord]
+
+			return selection
 		}
 
-		// put outside Selector?
+		, getRange: function ( a, b ) {
+			// return the list of words between (an including) A and B
+
+			if (!a || !b)
+				return []
+			
+			var words = Array.prototype.slice.call(document.querySelectorAll('.word'))
+				, ia = words.indexOf(a)
+				, ib = words.indexOf(b)
+
+			if (ia < ib) 
+				var first = ia, last = ib
+			else if (ia > ib) 
+				var first = ib, last = ia
+			else return [a]  // ia === ib
+
+			return words.slice(first, last+1)
+		}
+
+		, getSelectionEnds: function ( ) {
+			// returns the extreme words in current selection process.
+			if (!hoverWord || !endWord)
+				throw Error('why isn\'t hoverWord/endWord set?')
+			return [hoverWord, endWord]
+		}
+
 		, updateTweetBox: function () {
 			
-			var txt = ""
-				, w = Selector.getRange.apply(null, Selector.getSelectionEnds())
-			
-			console.log('words selected:', w)
-			for (var i=0; i<w.length; i++) {
-				txt += w[i].innerHTML + " "
+			var lines = Selector.getSplitRange.apply(null, Selector.getSelectionEnds())
+				, lpieces = []
+				, i = 0
+
+			while (line = lines[i++]) {
+				var wpieces = [], i2 = 0
+				while (word = line[i2++])
+					wpieces.push(word.innerHTML)
+				lpieces.push(wpieces.join(' '))
 			}
 
-			document.querySelector('textarea#tweet').value = txt
+			document.querySelector('textarea#tweet').value = lpieces.join(" ♬ ")  // ♪ ♫ ♩ ♬ ♭ ♮ ♯ 
 			updateTweetCounter()
 		}
 
@@ -114,20 +89,20 @@
 			// When words is null, use document.querySelectorAll('.word')?
 
 			function mouseoverWord ( e ) {
-				hoverWord = e.target;
+
+				hoverWord = e.target
 
 				if (!mouseDown)
 					return
 		
-				Selector.updateTweetBox()
-
 				if (!endWord)
 					// cursor, after clicked, moved over to a word.
 					// in that case, set endWord to be the first one "hovered".
-					endWord = e.target;
+					endWord = e.target
 
+				Selector.updateTweetBox()
 				Selector.unselectWords()
-				Selector.selectRange.apply(null, Selector.getSelectionEnds()) // (e.target, endWord)
+				Selector.selectRange.apply(null, Selector.getSelectionEnds())
 			}
 			
 			function mouseoutWord ( e ) {
@@ -141,7 +116,7 @@
 				words[i].addEventListener('mouseout', mouseoutWord)
 			}
 		}
-	};
+	}
 
 	mouseDown = false // mouse starts unclicked
 	endWord = null // the first word of a selection process, default to null
@@ -151,7 +126,7 @@
 		if (e.button != 0)
 			return // exit if it isn't a left-click
 
-		console.log(">> mousedown!", e)
+		console.log("=> mousedown")
 		mouseDown = true
 		
 		if (hoverWord) {
@@ -159,20 +134,20 @@
 			// cursor is ALREADY above a word (so select it right away)
 			// fire mouseover event to start selection of the current word
 			// otherwise it'll wait until the mouse goes over another word.
-			var e = document.createEvent("MouseEvents");
+			var e = document.createEvent("MouseEvents")
 			e.initMouseEvent("mouseover")
+			console.log('pseudo mouseover event fired on', hoverWord)
 			hoverWord.dispatchEvent(e)
 		}
 			
 	}
 
 	document.onmouseup = function ( e ) {		
-		if (e.button != 0)
-			return
-		
-		console.log(">> mouseup", e)
-		mouseDown = false
-		endWord = hoverWord = null
+		if (e.button == 0) { // left-click only
+			console.log("=> mouseup")
+			mouseDown = false
+			endWord = hoverWord = null
+		}
 	}
 
 })(window, window.document);
