091c986d832132d72a3c6158f123bee2dfd0e6cc
Gabriel Soldani <gabriel.soldani@gmail.com>
2012-07-29 14:57:19 -0300

    Search

--- search.js
+++ search.js
@@ -1,89 +1,89 @@
 ﻿function openTweetPopup () {
-	var text = document.querySelector('#tweet').value
-	window.open('https://twitter.com/intent/tweet?text='+escape(text))
+    var text = document.querySelector('#tweet').value;
+    window.open('https://twitter.com/intent/tweet?text='+encodeURIComponent(text));
 }
 
 function disableTweet () {
-	var counter = document.querySelector('.twtcounter')
-	counter.classList.add('exceed')
-	document.querySelector('#tweet-button').classList.add('disabled')
+    var counter = document.querySelector('.twtcounter');
+    counter.classList.add('exceed');
+    document.querySelector('#tweet-button').classList.add('disabled');
 }
 
 function enableTweet () {
-	var counter = document.querySelector('.twtcounter')
-	counter.classList.remove('exceed')
-	document.querySelector('#tweet-button').classList.remove('disabled')
+    var counter = document.querySelector('.twtcounter');
+    counter.classList.remove('exceed');
+    document.querySelector('#tweet-button').classList.remove('disabled');
 }
 
 function updateTweetCounter () {
-	// update characters counter in tweet textarea
-	var tweet =  document.querySelector('textarea#tweet').value,
-		counter = document.querySelector('.twtcounter')
-	
-	counter.innerHTML = tweet.length
-	if (tweet.length > 140)
-		disableTweet()
-	else enableTweet()
-	
-	return true;
+    // update characters counter in tweet textarea
+    var tweet =  document.querySelector('textarea#tweet').value;
+    var counter = document.querySelector('.twtcounter');
+    
+    counter.innerHTML = tweet.length;
+    if (tweet.length > 140)
+        disableTweet();
+    else enableTweet();
+    
+    return true;
 }
 
 function writeLyrics (text) {
-	// write lyrics given text retrieved from vagalume's api
-	// lines must be separated by '\n' and verses by empty lines
+    // write lyrics given text retrieved from vagalume's api
+    // lines must be separated by '\n' and verses by empty lines
 
-	var verses = text.split('\n\n');
-	var lyricstag = document.querySelector('.lyrics');
+    var verses = text.split('\n\n');
+    var lyricstag = document.querySelector('.lyrics');
 
-	// clean lyrics
-	while (lyricstag.children[0])
-		lyricstag.removeChild(lyricstag.children[0]);
+    // clean lyrics
+    while (lyricstag.children[0])
+        lyricstag.removeChild(lyricstag.children[0]);
 
-	// loop through verses
-	for (var v, i=0; v=verses[i]; i++) {
-		var vtag = document.createElement('div');
-		var lines = v.split('\n');
-		vtag.className = 'verse';
+    // loop through verses
+    for (var i = 0; i < verses.length; i++) {
+        var vtag = document.createElement('div');
+        var lines = verses[i].split('\n');
+        vtag.className = 'verse';
 
-		// loop through lines
-		for (var l, i2=0; l=lines[i2]; i2++) {
-			var ltag = document.createElement('div');
-			var words = l.split(' ');
-			ltag.className = 'line';
+        // loop through lines
+        for (var j = 0; j < lines.length; j++) {
+            var ltag = document.createElement('div');
+            var words = lines[j].split(' ');
+            ltag.className = 'line';
 
-			// loop through words
-			for (var w, i3=0; w=words[i3]; i3++) {
-				var wtag = document.createElement('span');
-				wtag.className = 'word';
-				wtag.innerHTML = w;
+            // loop through words
+            for (var k = 0; k < words.length; k++) {
+                var wtag = document.createElement('span');
+                wtag.className = 'word';
+                wtag.innerHTML = words[k];
 
-				ltag.appendChild(wtag);
-			}
+                ltag.appendChild(wtag);
+            }
 
-			vtag.appendChild(ltag);
-		}
+            vtag.appendChild(ltag);
+        }
 
-		lyricstag.appendChild(vtag);
-	}
+        lyricstag.appendChild(vtag);
+    }
 
-	Selector.addSelectionEvent();
+    Selector.addSelectionEvent();
 }
 
 (function () {
-	var tweet = document.querySelector('textarea#tweet');
-	tweet.addEventListener('focus', updateTweetCounter);
-	tweet.addEventListener('keyup', updateTweetCounter);
-	tweet.addEventListener('onchange', updateTweetCounter);
+    var tweet = document.querySelector('textarea#tweet');
+    tweet.addEventListener('focus', updateTweetCounter);
+    tweet.addEventListener('keyup', updateTweetCounter);
+    tweet.addEventListener('onchange', updateTweetCounter);
 })();
 
 vagalume.musicInfoFromName(
-	$('#search-artist').val(),
-	$('#search-music').val(),
-	function () {
-		console.log('oncaptcha', arguments);
-	}, function (data) {
-		writeLyrics(data.music[0].lyrics);
-		$('#music-name').html(data.music[0].name);
-		$('#artist-name').html(data.artist.name);
-	}
+    $('#search-artist').val(),
+    $('#search-music').val(),
+    function () {
+        console.log('oncaptcha', arguments);
+    }, function (data) {
+        writeLyrics(data.music[0].lyrics);
+        $('#music-name').html(data.music[0].name);
+        $('#artist-name').html(data.artist.name);
+    }
 );
